f34ef4528ee919aa2702f7f7e2bc3ce35d99d2da
Rahul-Agarwal-03 <rahul03agarwal02@gmail.com>
2021-06-19 11:42:06 +0530

    STC FINAL JS TASK

--- movie.js
+++ movie.js
@@ -1,9 +1,11 @@
 const form=document.querySelector("form");
-const email=form.elements.email;
-const movie=form.elements.movie;
-const ticket = form.elements.ticket;
+let email=form.elements.email;
+let movie=form.elements.movie;
+let ticket = form.elements.ticket;
 const booking = document.querySelector(".booking");
 const table= document.createElement("table");
+const row=document.createElement("tr");
+let customer=0;
 function calculatePrice(t)
 {
     if(t==="platinum")
@@ -19,9 +21,16 @@ function calculatePrice(t)
         return "80Rs"
     }
 }
+
+booking.appendChild(table);
+document.querySelector("table").innerHTML="<th>S No.</th><th>Name</th><th>Email Id</th><th>Movie</th><th>Ticket</th><th>Price</th>";
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    booking.appendChild(table);
-    document.querySelector("table").innerHTML="<th>Name</th><th>Email Id</th><th>Movie</th><th>Ticket</th><th>Price</th>";
-    document.querySelector("table").insertRow(1).innerHTML=`<td>${form.elements.name.value}</td><td>${email.value}</td><td>${movie.value}</td><td>${ticket.value}</td><td>${calculatePrice(ticket.value)}</td>`;
+    customer++;
+    document.querySelector("table").insertRow(customer).innerHTML=`<td>${customer}</td><td>${form.elements.name.value}</td><td>${email.value}</td><td>${movie.value}</td><td>${ticket.value}</td><td>${calculatePrice(ticket.value)}</td>`;
+    form.elements.name.value="";
+    email.value="";
+    movie.value="";
+    ticket.value="";
+    
 })
